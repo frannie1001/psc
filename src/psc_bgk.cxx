9e911025fd0944d6a25aef7098f917bc89632510
frannie1001 <105309881+frannie1001@users.noreply.github.com>
2024-03-04 20:11:59 -0900

    put . on some numbers

--- src/psc_bgk.cxx
+++ src/psc_bgk.cxx
@@ -269,8 +269,8 @@ double v_phi_cdf(double v_phi, double rho)
 
   double rho_sqr = sqr(rho);
 
-  double gamma = 1 + 8 * g.k * rho_sqr;
-  double alpha = 1 - g.h0 / std::sqrt(gamma) *
+  double gamma = 1. + 8 * g.k * rho_sqr;
+  double alpha = 1. - g.h0 / std::sqrt(gamma) *
                        std::exp(-g.k * sqr(B) * sqr(rho_sqr) / gamma);
 
   double mean0 = 0;
@@ -354,7 +354,7 @@ void initializeParticles(Balance& balance, Grid_t*& grid_ptr, Mparticles& mprts,
           double py = coef * g.m_e * -vphi * z / rho;
           double px = coef * g.m_e * 4/3;  //hard coded for Az = 2, xi = 1
           np.p = setup_particles.createMaxwellian(
-            {np.kind, np.n, {0, py, pz}, {1, getTey(rho,z), getTez(rho,y)}, np.tag});
+            {np.kind, np.n, {0, py, pz}, {1., getTey(rho,z), getTez(rho,y)}, np.tag});
         } else {
           np.p = pdist(y, z, rho);
         }
