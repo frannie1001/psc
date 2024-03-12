964d81e1d37cfd3a93ec19b5fc553da8408925c1
frannie1001 <105309881+frannie1001@users.noreply.github.com>
2024-03-12 13:23:38 -0800

    new form of v_phi_maxwellian

--- src/psc_bgk.cxx
+++ src/psc_bgk.cxx
@@ -297,6 +297,40 @@ double v_phi_cdf(double v_phi, double rho)
 
   return m0 / alpha + m1 * (1 - 1 / alpha);
 }
+// ======================================================================
+// v_phi_maxwellian
+// trying to make a maxwellian for v_phi
+
+double v_phi_maxwellian_mean(double rho)
+{
+  // convert units from psc to paper
+  rho /= get_beta();
+
+  double B = g.Hx;
+  double A_phi = B*rho/2;
+  double rho_sqr = sqr(rho);
+
+  double gamma = 1. + 8 * g.k * rho_sqr;
+  double numerator = 8 * g.k * rho_sqr * A_phi;
+  
+
+  return numerator/gamma;
+}
+
+double v_phi_maxwellian_stdev(double rho)
+{
+  // convert units from psc to paper
+  rho /= get_beta();
+
+  double B = g.Hx;
+  double A_phi = B*rho/2;
+  double rho_sqr = sqr(rho);
+
+  double gamma = 1. + 8 * g.k * rho_sqr;
+  
+
+  return 1/std::sqrt(gamma);
+}
 
 struct pdist
 {
@@ -304,7 +338,7 @@ struct pdist
     : y{y},
       z{z},
       rho{rho},
-      v_phi_dist{0, get_beta()},
+      v_phi_dist{v_phi_maxwellian_mean(), get_beta()* v_phi_maxwellian_stdev()},
       ///v_phi_dist{[=](double v_phi) { return v_phi_cdf(v_phi, rho); }}, //change me
       v_rho_dist{0, get_beta()}, //mean, std dev
       v_x_dist{0, get_beta()}
