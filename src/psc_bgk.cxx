dd285edf3ba898939063ba0a6d33ee6ccfc20732
frannie1001 <105309881+frannie1001@users.noreply.github.com>
2024-03-13 14:36:08 -0800

    1.01 ion density

--- src/psc_bgk.cxx
+++ src/psc_bgk.cxx
@@ -388,7 +388,7 @@ void initializeParticles(Balance& balance, Grid_t*& grid_ptr, Mparticles& mprts,
     switch (kind) {
 
       case ELECTRON_SECOND:
-        np.n = 1 - qDensity(idx[0], idx[1], idx[2], 0, p) - getBackgroundDensity(rho);
+        np.n = 1.01 - qDensity(idx[0], idx[1], idx[2], 0, p) - getBackgroundDensity(rho);
         if (rho == 0) {
           double Te = parsedData->get_interpolated(COL_TE, rho);
           np.p = setup_particles.createMaxwellian(
@@ -408,7 +408,7 @@ void initializeParticles(Balance& balance, Grid_t*& grid_ptr, Mparticles& mprts,
         break;
 
       case ION_KIND:
-        np.n = 1;
+        np.n = 1.01;
         np.p = setup_particles.createMaxwellian(
           {np.kind, np.n, {0, 0, 0}, {Ti, Ti, Ti}, np.tag});
         break;
