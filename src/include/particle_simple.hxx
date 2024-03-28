f1014d5a33784113417ef95a2afc38c64c6e2c9c
frannie1001 <105309881+frannie1001@users.noreply.github.com>
2024-03-28 05:08:36 -0800

    added tags to particle_simple

--- src/include/particle_simple.hxx
+++ src/include/particle_simple.hxx
@@ -17,7 +17,7 @@ struct ParticleSimple
 
   KG_INLINE ParticleSimple(Real3 x, Real3 u, real_t qni_wni, int kind,
                            psc::particle::Id id, psc::particle::Tag tag)
-    : x{x}, u{u}, kind{kind}, qni_wni{qni_wni}
+    : x{x}, u{u}, kind{kind}, qni_wni{qni_wni}, tag{tag}
   {}
 
   KG_INLINE bool operator==(const ParticleSimple& other) const
@@ -32,13 +32,14 @@ struct ParticleSimple
   }
 
   KG_INLINE psc::particle::Id id() const { return 0; }
-  KG_INLINE psc::particle::Tag tag() const { return 0; }
+  KG_INLINE psc::particle::Tag tag() const { return tag; }
 
 public:
   Real3 x;
   Real3 u;
   int kind;
   real_t qni_wni;
+  int tag;
 };
 
 template <typename R>
